0da7a26c1c818d9c2812fb7ea48178f644ac7121
Araggik <45770047+Araggik@users.noreply.github.com>
2021-09-26 20:42:08 +0500

    Add files via upload

--- Dz/PyMath/PyMath.py
+++ Dz/PyMath/PyMath.py
@@ -1,6 +1,9 @@
 from math import sin,exp
 import numpy as np
 from scipy.linalg import solve
+import matplotlib.pyplot as plt
+
+# Task1
 
 def f(x):
     return sin(x / 5) * exp(x / 10) + 5 * exp(-x / 2)
@@ -19,6 +22,33 @@ def solve_sys(vec):
     nvector_b = np.array(vector_b)
     return solve(narray_a,nvector_b)
 
-vector_a = [1, 15]
+def calculate(sol,point):
+    sum = 0
+    i = 0
+    for el in sol:
+        sum += el * pow(point,i)
+        i += 1
+    return sum
 
+def get_values(vec,solut):
+    list = []
+    for el in vec:
+        list.append(calculate(solut,el))
+    return list
 
+#vector_a = [1, 15]
+#vector_a = [1, 8, 15]
+vector_a = [1, 4 , 10 ,15]
+
+solution = solve_sys(vector_a)
+vector_y = get_values(vector_a,solution)
+
+plt.plot(vector_a,vector_y)
+plt.show()
+
+out_file = open('submission-2.txt', 'w')
+for w in solution:
+    out_file.write(str(w) + ' ')
+out_file.close()
+
+# Task2
